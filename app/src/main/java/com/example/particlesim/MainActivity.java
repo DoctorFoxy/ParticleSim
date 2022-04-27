41d54691b2d1e05feecdf3b225dd3e2de2d361fa
Maarten <mmedaer@gmail.com>
2022-04-27 15:03:19 +0200

    Added click to "spawn" new particle and selectedParticle attribute.

--- app/src/main/java/com/example/particlesim/MainActivity.java
+++ app/src/main/java/com/example/particlesim/MainActivity.java
@@ -2,17 +2,16 @@ package com.example.particlesim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
-
-import java.util.Timer;
-import java.util.TimerTask;
 
 import ParticleEngine.EmptyParticle;
 import ParticleEngine.Particle;
@@ -31,15 +30,18 @@ public class MainActivity extends AppCompatActivity {
 
     private int backgroundColor;
     private int simSpeed;
+    private Type selectedType;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         //Config
-        backgroundColor = Color.TRANSPARENT;
+        backgroundColor = Color.GREEN;
         simSpeed = 50;
+        selectedType = Type.SAND;
 
         //
         gameBitmap = Bitmap.createBitmap(350,350, Bitmap.Config.ARGB_8888);
@@ -52,9 +54,12 @@ public class MainActivity extends AppCompatActivity {
         //Testing stuff
         particleWorld = new ParticleWorld(35,35);
         Particle sand = new SandParticle(particleWorld);
-        particleWorld.setParticle(2,6, new StoneParticle(particleWorld));
+        //particleWorld.setParticle(2,6, new StoneParticle(particleWorld));
         particleWorld.setParticle(2,2, sand);
         particleWorld.setParticle(0,0, sand);
+        particleWorld.setParticle(4,0, sand);
+        particleWorld.setParticle(6,0, sand);
+        particleWorld.setParticle(7,0, sand);
         particleWorld.setParticle(0,1, sand);
         particleWorld.setParticle(9,0, sand);
         particleWorld.setParticle(14,0, sand);
@@ -65,6 +70,22 @@ public class MainActivity extends AppCompatActivity {
         //Repeats code
         tickHandler = new Handler();
         repeatedCode.run();
+
+        //Imageview Listener
+        imageViewGame.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    int x = (int) event.getX();
+                    int parsedX = (int) Math.floor(x/(imageViewGame.getWidth()/35));
+                    int y = (int) event.getY();
+                    int parsedY = (int) Math.floor(y/(imageViewGame.getHeight()/35));
+
+                    newParticle(parsedX, parsedY, selectedType);
+                }
+                return true;
+            }
+        });
     }
 
     Runnable repeatedCode = new Runnable() {
@@ -73,15 +94,37 @@ public class MainActivity extends AppCompatActivity {
             particleWorld.doPhysics();
             refresh();
 
-            System.out.println("refreshed");
+            //System.out.println("refreshed");
 
             tickHandler.postDelayed(repeatedCode, simSpeed);
         }
     };
 
+    public void newParticle(int x, int y, Type type) {
+        Particle newParticle = new EmptyParticle(particleWorld);
+        switch (type) {
+            case EMPTY:
+                new EmptyParticle(particleWorld);
+                break;
+            case STONE:
+                newParticle = new StoneParticle(particleWorld);
+                break;
+            case SAND:
+                newParticle = new SandParticle(particleWorld);
+                break;
+            default: newParticle = new EmptyParticle(particleWorld);
+            //case GAS:   newParticle = new GasParticle(particleWorld
+                //TODO add other particleTypes
+        }
+
+        particleWorld.setParticle(x,y,newParticle);
+        drawParticle(x,y,newParticle.getColor());
+    }
+
     public void drawParticle(int x, int y, int color) {
         paint.setColor(color);
         canvas.drawRect(x*10,y*10,x*10+10,y*10+10, paint);
+        imageViewGame.invalidate();
     }
 
     public void clearGame() {
