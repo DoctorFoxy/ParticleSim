d829884044d901ffe804ae96243ce941e28bb637
Maarten <mmedaer@gmail.com>
2022-05-11 16:53:51 +0200

    Added upload capabilities.

--- app/src/main/java/StartScreen/UploadScreen.java
+++ app/src/main/java/StartScreen/UploadScreen.java
@@ -6,12 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.particlesim.MainActivity;
@@ -25,6 +27,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UploadScreen extends AppCompatActivity {
     private RequestQueue requestQueue;
@@ -35,7 +39,7 @@ public class UploadScreen extends AppCompatActivity {
         setContentView(R.layout.activity_upload_screen);
     }
 
-    public void onUploadPressed(View caller){
+    public void onUploadPressed(View caller) throws JSONException {
 
         // TODO: UPLOAD DATABASE STUFF
         upload();
@@ -49,33 +53,23 @@ public class UploadScreen extends AppCompatActivity {
 
     public void upload() {
         requestQueue = Volley.newRequestQueue( this );
-        String postURL = "https://studev.groept.be/api/a21pt211/getAll";
+        String requestURL = "https://studev.groept.be/api/a21pt211/uploadWorld/";
 
-        String WorldName = findViewById(R.id.editWorldNameInput).toString();
-        String Username = findViewById(R.id.editTextUsernameInput).toString();
+        EditText textWorldName = (EditText)findViewById(R.id.editTextWorldName);
+        String WorldName = textWorldName.getText().toString();
 
-        StringRequest submitRequest = new StringRequest(Request.Method.POST, postURL,
+        EditText textUsername = (EditText)findViewById(R.id.editTextUsername);
+        String Username = textUsername.getText().toString();
+        System.out.println(getIntent().getSerializableExtra("World"));
+        requestURL += Username + "/" + WorldName + "/" + getIntent().getSerializableExtra("World");
 
-                new Response.Listener<String>()
+        JsonArrayRequest submitRequest = new JsonArrayRequest(Request.Method.GET, requestURL, null,
+
+                new Response.Listener<JSONArray>()
                 {
                     @Override
-                    public void onResponse(String response)
-                    {
-                        try {
-                            JSONArray responseArray = new JSONArray(response);
-                            String responseString = "";
-                            for( int i = 0; i < responseArray.length(); i++ )
-                            {
-                                JSONObject curObject = responseArray.getJSONObject( i );
-                                responseString += curObject.getString( "Username" ) + " : " + curObject.getString( "WorldName" ) + "\n";
-                            }
-
-                            System.out.println(responseString);
-                        }
-                        catch( JSONException e )
-                        {
-                            Log.e( "Database", e.getMessage(), e );
-                        }
+                    public void onResponse(JSONArray response) {
+                        System.out.println("SUCCESS");
                     }
                 },
 
@@ -85,6 +79,8 @@ public class UploadScreen extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error)
                     {
                         //txtResponse.setText( error.getLocalizedMessage() );
+                        //TODO: Error handler upload
+                        System.out.println("ERROR");
                     }
                 }
         );
